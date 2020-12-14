03c05ecdabf50bedbe1268de726dc10571c0ee2e
Yash Goyal <yashgoyalcs@gmail.com>
2020-12-14 19:51:25 +0530

    modify templates and views

--- src/application.py
+++ src/application.py
@@ -1,3 +1,3 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
--- src/views/views.py
+++ src/views/views.py
@@ -1,9 +1,26 @@
-from application import app
 from flask import render_template
+from application import app
+
 
 @app.route('/')
 def slink():
     return render_template('slink.html')
 
+
+@app.route('/panel')
+def panel():
+    return render_template('panel.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
